33ae7905bb9b85fa6395fb002c2bc5c6594e73b3
michelle <foleymichelle9@gmail.com>
2020-07-06 17:36:35 -0500

    Finish test and update view

--- spec/features/vistors/visitor_cannot_see_classroom_content_spec.rb
+++ spec/features/vistors/visitor_cannot_see_classroom_content_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 describe 'visitor visits video show page' do
   it 'Cannot see tutorials labeled classroom content' do
-    tutorial = create(:tutorial)
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial, description: "Amazing Much Learning!", classroom: true)
 
+      # video1 = create(:video, tutorial_id: tutorial1.id)
+      # video2 = create(:video, tutorial_id: tutorial1.id)
+      # video3 = create(:video, tutorial_id: tutorial2.id)
+      # video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      within(first('.tutorials')) do
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+
+        expect(page).to_not have_content(tutorial2.title)
+        expect(page).to_not have_content(tutorial2.description)
+    end
   end
 end
 
