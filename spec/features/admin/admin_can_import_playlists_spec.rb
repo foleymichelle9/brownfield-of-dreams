23f96bcce4aab238a5a0514394b7d41fe1fd789b
Melanie Tran <melaniexx02@gmail.com>
2020-07-07 14:07:41 -0600

    Made test more robust

--- spec/features/admin/admin_can_import_playlists_spec.rb
+++ spec/features/admin/admin_can_import_playlists_spec.rb
@@ -42,15 +42,15 @@ describe "Admin Tutorials" do
     fill_in :thumbnail, with: "Thumbnail"
 
     click_on "Save"
-    
+
     expect(page).to have_content("Title can't be blank")
   end
 
-  scenario "Admin can import youtube playlist with more than 50 videos" do
+  scenario "Admin can import youtube playlist with 50 videos" do
     visit new_admin_playlist_path
 
-    fill_in :playlist_id, with: "PLD8iUdp33PqSmH4NjDm6lk1YiNUhLCxj4"
-    fill_in :title, with: "BuzzFeed Unsolved SuperNatural"
+    fill_in :playlist_id, with: "PLD7nPL1U-R5pSwKIcVaIQrG5BnGMbHI5H"
+    fill_in :title, with: "Drunk History"
     fill_in :description, with: "Hey There Demons"
     fill_in :thumbnail, with: "Thumbnail"
 
@@ -60,5 +60,8 @@ describe "Admin Tutorials" do
     expect(page).to have_content("Successfully created tutorial.")
 
     click_on "View it here"
+
+    tutorial = Tutorial.last
+    expect(tutorial.videos.count).to eq(50)
   end
 end
