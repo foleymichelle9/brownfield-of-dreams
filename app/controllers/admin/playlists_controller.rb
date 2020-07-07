dadc1a3ba5c7b85049d0b1e3c75f51bc257dedf9
Melanie Tran <melaniexx02@gmail.com>
2020-07-07 06:04:08 -0600

    Obtains video data but cant create video objects

--- app/controllers/admin/playlists_controller.rb
+++ app/controllers/admin/playlists_controller.rb
@@ -4,25 +4,20 @@ class Admin::PlaylistsController < Admin::BaseController
   end
 
   def create
-    # tutorial = Tutorial.new(playlist_params)
-    json = YoutubeService.new.playlist_info(params[:playlist_id])
-    video_params = {}
-    json[:items].each do |video_data|
-      video_params[:id] = video_data[:id]
-      video_params[:title] = video_data[:snippet][:title]
-      video_params[:description] = video_data[:snippet][:description]
-      video_params[:thumbnail] = video_data[:snippet][:thumbnails][:default]
+    tutorial = Tutorial.new(playlist_params)
+    tutorial.save
+    playlist_data = YoutubeService.new.playlist_info(params[:playlist_id])
+    playlist_data[:items].each do |video_data|
+      video_params = {
+        :id => video_data[:id],
+        :title => video_data[:snippet][:title],
+        :description => video_data[:snippet][:description],
+        :thumbnail => video_data[:snippet][:thumbnails][:default]
+      }
+      bob = Video.create(video_params)
     end
-
-    tutorial = Tutorial.create(video_params)
-
-    if tutorial.save
       flash[:notice] = "Successfully created tutorial. #{view_context.link_to("View it here", tutorial_path(tutorial.id))}"
       redirect_to admin_dashboard_path
-    else
-      flash[:error] = tutorial.errors.full_messages.to_sentence
-      render :new
-    end
   end
 
   private
