fe68cfd5a890eaa626d6708004949cc2d69d393c
Melanie Tran <melaniexx02@gmail.com>
2020-07-09 07:06:07 -0600

    Change token in test

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User Has Friends through Github" do
   it "can not add a friend if user does not have account" do
-    user1 = create(:user, github_token: ENV['GITHUB_MEL_TOKEN'], github_username: "melatran")
+    user1 = create(:user, github_token: ENV['GITHUB_ACCESS_TOKEN'], github_username: "sagemlee")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
@@ -12,21 +12,21 @@ describe "User Has Friends through Github" do
       expect(page).to have_content("You have no friends.")
     end
 
-    within "#following-sagemlee" do
-      expect(page).to have_content("sagemlee")
+    within "#following-melatran" do
+      expect(page).to have_content("melatran")
       expect(page).to_not have_content("Add Friend")
     end
   end
 
-  it "can add friend if they have an account" do
-    user1 = create(:user, github_token: ENV['GITHUB_MEL_TOKEN'], github_username: "melatran")
-    user2 = create(:user, github_token: ENV['GITHUB_ACCESS_TOKEN'], github_username: "sagemlee")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-
-    stub_omniauth
-
-    visit "/dashboard"
-    click_link "Connect to GitHub"
-
-  end
+  # it "can add friend if they have an account" do
+  #   user1 = create(:user, github_token: ENV['GITHUB_MEL_TOKEN'], github_username: "melatran")
+  #   user2 = create(:user, github_token: ENV['GITHUB_ACCESS_TOKEN'], github_username: "sagemlee")
+  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+  #
+  #   stub_omniauth
+  #
+  #   visit "/dashboard"
+  #   click_link "Connect to GitHub"
+  #
+  # end
 end
