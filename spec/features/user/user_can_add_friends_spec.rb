1a692706d83afe6db70534022002d9a03efa3201
Melanie Tran <melaniexx02@gmail.com>
2020-07-08 22:45:18 -0600

    Test gitignore

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -2,16 +2,13 @@ require 'rails_helper'
 
 describe "User Has Friends through Github" do
   it "can add friends" do
-    # json_response = File.read('spec/fixtures/melanies_followers.json')
-    # stub_request(:get, "https://api.github.com/user/followers").
-    # to_return(status: 200, body: json_response)
     user1 = create(:user, github_token: ENV['GITHUB_MEL_TOKEN'], github_username: "melatran")
     user2 = create(:user, github_token: ENV['GITHUB_ACCESS_TOKEN'], github_username: "sagemlee")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
     visit dashboard_path
-  
+
     within ".friends" do
       expect(page).to have_content("You have no friends.")
     end
